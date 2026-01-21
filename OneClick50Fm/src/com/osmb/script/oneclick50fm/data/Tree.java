d71fd28aee59e5af12c86678235dd6d60048ff67
Jose OSMB <accsecundariaprodead@gmail.com>
2026-01-20 22:27:06 -0400

    Version 1.04

--- OneClick50Fm/src/com/osmb/script/oneclick50fm/data/Tree.java
+++ OneClick50Fm/src/com/osmb/script/oneclick50fm/data/Tree.java
@@ -1,27 +1,23 @@
 package com.osmb.script.oneclick50fm.data;
 
 import com.osmb.api.item.ItemID;
-import com.osmb.api.location.area.Area;
 import com.osmb.api.visual.SearchablePixel;
 
-
 public enum Tree {
-    NORMAL("Tree", ItemID.LOGS, PixelProvider.TREE_CLUSTER_1, AreaManager.TREE_AREA, 1),
-    OAK("Oak tree", ItemID.OAK_LOGS, PixelProvider.TREE_CLUSTER_1, AreaManager.OAK_TREE_AREA, 15),
-    WILLOW("Willow tree", ItemID.WILLOW_LOGS, PixelProvider.TREE_CLUSTER_1, AreaManager.WILLOW_TREE_AREA, 30),;
+    NORMAL("Tree", ItemID.LOGS, PixelProvider.TREE_CLUSTER_1, 1),
+    OAK("Oak tree", ItemID.OAK_LOGS, PixelProvider.TREE_CLUSTER_1, 15),
+    WILLOW("Willow tree", ItemID.WILLOW_LOGS, PixelProvider.TREE_CLUSTER_1, 30);
 
     private final int logID;
     private final SearchablePixel[] cluster;
     private final String objectName;
     private final int requiredLevel;
-    private final Area treeArea;
 
-    Tree(String objectName, int logID, SearchablePixel[] cluster, Area treeArea, int requiredLevel) {
+    Tree(String objectName, int logID, SearchablePixel[] cluster, int requiredLevel) {
         this.objectName = objectName;
         this.logID = logID;
         this.cluster = cluster;
         this.requiredLevel = requiredLevel;
-        this.treeArea = treeArea;
     }
 
     public static Tree getTreeForLevel(int level) {
@@ -31,11 +27,7 @@ public enum Tree {
                 return trees[i];
             }
         }
-        return null; // No tree found for the given level
-    }
-
-    public Area getTreeArea() {
-        return treeArea;
+        return null;
     }
 
     public int getRequiredLevel() {
@@ -53,5 +45,4 @@ public enum Tree {
     public SearchablePixel[] getCluster() {
         return cluster;
     }
-
 }
